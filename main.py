2a0a56e1de64f7e08d776dd472af01c890a782d3
StruDJA <strubot@gmail.com>
2021-01-26 23:36:07 +0000

    Create main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import jwt
 from flask import Flask, jsonify, request, abort
 
 
-JWT_SECRET = os.environ.get('JWT_SECRET', 'MyJwtSecret')
+JWT_SECRET = os.environ.get('JWT_SECRET', 'MyJWTSecret')
 LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO')
 
 
